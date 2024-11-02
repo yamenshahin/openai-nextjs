4d4689da5b7d4783411e9378ce8bb128e7a2387b
Yamen Shahin <yamenshahin@gmail.com>
2024-11-02 22:20:06 +0200

    Remove: Footer components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Header from './components/Header';
 import Main from './components/Main';
-import Footer from './components/Footer';
 
 const Home = () => {
   return (
     <>
       <Header />
       <Main />
-      <Footer />
     </>
   );
 };
