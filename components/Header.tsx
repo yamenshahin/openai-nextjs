945ca0c36e2998afb96b2632d6d02676eb4353b3
Yamen Shahin <yamenshahin@gmail.com>
2024-11-04 06:40:08 +0200

    Style: sign out button and header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,25 @@
 import { signOut } from '../auth';
+import { FaSignOutAlt } from 'react-icons/fa';
+
 const Header = () => {
   return (
-    <>
-      <header>
-        <h1>This is header</h1>
-        <form
-          action={async () => {
-            'use server';
-            await signOut();
-          }}
+    <header className="flex justify-between items-center">
+      <h1 className="text-2xl font-bold">This is header</h1>
+      <form
+        action={async () => {
+          'use server';
+          await signOut();
+        }}
+      >
+        <button
+          type="submit"
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded flex items-center"
         >
-          <button type="submit">Sign Out</button>
-        </form>
-      </header>
-    </>
+          Sign Out <FaSignOutAlt className="ml-2" />
+        </button>
+      </form>
+    </header>
   );
 };
+
 export default Header;
