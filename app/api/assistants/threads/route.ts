e4bf90400a35564748b878699e39c3d2de719965
Yamen Shahin <yamenshahin@gmail.com>
2024-11-06 08:19:13 +0200

    Make code consisting

--- app/api/assistants/threads/route.ts
+++ app/api/assistants/threads/route.ts
@@ -3,7 +3,9 @@ import { openai } from '@/app/openai';
 export const runtime = 'nodejs';
 
 // Create a new thread
-export async function POST() {
+const POST = async () => {
   const thread = await openai.beta.threads.create();
   return Response.json({ threadId: thread.id });
-}
+};
+
+export { POST };
