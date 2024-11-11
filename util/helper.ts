f985035c545fc2abb165846d5778ee2385bf526d
Yamen Shahin <yamenshahin@gmail.com>
2024-11-11 16:27:46 +0200

    Fix: ts

--- util/helper.ts
+++ util/helper.ts
@@ -1,9 +1,11 @@
-const emailToUsername = (email) => {
+const emailToUsername = (email: string) => {
   return email.split('@')[0];
 };
 
-const findIndexOfThread = (array, id) => {
+const findIndexOfThread = (array: unknown[], id: string) => {
   if (!array) return -1;
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
   return array.findIndex((obj) => obj.threadId === id);
 };
 
