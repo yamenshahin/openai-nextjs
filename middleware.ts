2f00b708f5163009d3045f3d5c929ac592d0b801
Yamen Shahin <yamenshahin@gmail.com>
2024-11-11 14:54:44 +0200

    Fix: mongoose and edge runtime

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,9 @@
+import { runtime } from './app/api/assistants/threads/route';
+
 export { auth as middleware } from '@/auth';
+
+export const config = {
+  matcher: '/:path*',
+  runtime: 'nodejs',
+  unstable_allowDynamic: ['/lib/db.ts', '/node_modules/mongoose/dist/**'],
+};
