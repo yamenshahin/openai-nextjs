9e444f213be37c56ae83f78112acc1dc82a989b0
Yamen Shahin <yamenshahin@gmail.com>
2024-11-12 11:14:35 +0200

    Add: sort threads by time

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,12 +6,16 @@ interface Thread {
 }
 
 const Sidebar = ({ userThreads }: { userThreads: Thread[] }) => {
+  let reversedUserThreads: Thread[] = [];
+  if (userThreads) {
+    reversedUserThreads = userThreads.reverse();
+  }
   return (
     <>
       <div className="w-[420px] bg-gray-100 p-4 h-full">
         <ul>
-          {userThreads &&
-            userThreads.map((thread) => (
+          {reversedUserThreads &&
+            reversedUserThreads.map((thread) => (
               <li key={thread.threadId}>
                 <Link href={{ pathname: `/threads/${thread.threadId}` }}>
                   {thread.threadTitle}
