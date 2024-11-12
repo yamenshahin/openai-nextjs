50e2457c9d6b5976754fb1b922ab7d00a580e189
Yamen Shahin <yamenshahin@gmail.com>
2024-11-12 11:28:29 +0200

    Style: sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { FaCommentAlt } from 'react-icons/fa';
 
 interface Thread {
   threadId: string;
@@ -13,11 +14,15 @@ const Sidebar = ({ userThreads }: { userThreads: Thread[] }) => {
   return (
     <>
       <div className="w-[420px] bg-gray-100 p-4 h-full">
-        <ul>
+        <ul className="list-none p-0">
           {reversedUserThreads &&
             reversedUserThreads.map((thread) => (
-              <li key={thread.threadId}>
-                <Link href={{ pathname: `/threads/${thread.threadId}` }}>
+              <li key={thread.threadId} className="px-4 py-2 hover:bg-gray-200">
+                <Link
+                  href={{ pathname: `/threads/${thread.threadId}` }}
+                  className="inline-block align-middle"
+                >
+                  <FaCommentAlt className="text-gray-500 mr-2 inline-block align-middle" />
                   {thread.threadTitle}
                 </Link>
               </li>
